ff3edd56e6055f2894af1784bb27c2cbc489976c
koktoh <koktoh96@gmail.com>
2020-06-25 19:32:10 +0900

    replace textboxes to constrained textboxes

---
+++ address-label-utility/Views/IntegerTextBox.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows.Controls;
+
+namespace AddressLabelUtility.Views
+{
+    public class IntegerTextBox : TextBox
+    {
+        public IntegerTextBox()
+        {
+            this.PreviewTextInput += this.IntegerTextBox_PreviewTextInput;
+        }
+
+        private void IntegerTextBox_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            e.Handled = !Regex.IsMatch(e.Text, "[0-9]"); 
+        }
+    }
+}
---
+++ address-label-utility/Views/NumericTextBox.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows.Controls;
+
+namespace AddressLabelUtility.Views
+{
+    public class NumericTextBox : TextBox
+    {
+        public NumericTextBox()
+        {
+            this.PreviewTextInput += this.NumericTextBox_PreviewTextInput;
+        }
+
+        private void NumericTextBox_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            if (this.Text.Contains(".") && e.Text == ".")
+            {
+                e.Handled = true;
+                return;
+            }
+
+            e.Handled = !Regex.IsMatch(e.Text, @"[0-9\.]");
+        }
+    }
+}
